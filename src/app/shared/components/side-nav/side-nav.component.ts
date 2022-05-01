47545f666f4b049b2720c5115936cb438334a4bc
Utkarsh Raj <utkarsh.raj@outlook.com>
2022-05-01 14:02:28 +0530

    refactoring the ui and reversing some designs

--- src/app/shared/components/side-nav/side-nav.component.ts
+++ src/app/shared/components/side-nav/side-nav.component.ts
@@ -4,18 +4,14 @@ import { ISideNavRouteDetails } from '../../models/sideNavRouteDetails.model';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
-
   @Input('sidenav') sideNavRouteDetails: ISideNavRouteDetails[];
 
   routeDetails: ISideNavRouteDetails[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-    this.routeDetails = this.sideNavRouteDetails;
-  }
-
+  ngOnInit(): void {}
 }
