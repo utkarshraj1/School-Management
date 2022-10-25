02fb9eed51a2221aa05229d0668c2563efa8d25f
Utkarsh Raj <utkarsh.raj@outlook.com>
2022-10-25 15:04:36 +0530

    Feature : Updating the folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,43 +6,43 @@ const routes: Routes = [
   {
     path: 'student',
     loadChildren: () =>
-      import('./components/student/student.module').then(
-        (m) => m.StudentModule,
-      ),
+      import('./features/student/student.module').then((m) => m.StudentModule),
   },
   {
     path: 'analyst',
     loadChildren: () =>
-      import('./components/data-analyst/data-analyst.module').then(
+      import('./features/data-analyst/data-analyst.module').then(
         (m) => m.DataAnalystModule,
       ),
   },
   {
     path: 'teacher',
     loadChildren: () =>
-      import('./components/teacher/teacher.module').then(
-        (m) => m.TeacherModule,
-      ),
+      import('./features/teacher/teacher.module').then((m) => m.TeacherModule),
   },
   {
     path: 'accounts',
     loadChildren: () =>
-      import('./components/accountant/accountant.module').then(
+      import('./features/accountant/accountant.module').then(
         (m) => m.AccountantModule,
       ),
   },
   {
     path: 'super',
     loadChildren: () =>
-      import('./components/super-user/super-user.module').then(
+      import('./features/super-user/super-user.module').then(
         (m) => m.SuperUserModule,
       ),
   },
   {
     path: '',
     loadChildren: () =>
-      import('./components/login/login.module').then((m) => m.LoginModule),
+      import('./features/login/login.module').then((m) => m.LoginModule),
     pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent,
   },
 ];
 
